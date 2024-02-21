895588c4a5fe85b4d032183d76dbf4732b4709a7
LipeCortez <75758075+LipeCortez@users.noreply.github.com>
2024-02-21 23:51:08 +0000

    Modifiquei

--- S2programa2.c
+++ S2programa2.c
@@ -6,6 +6,6 @@ int main() {
   printf("Mackenzie - FCI\n");
   printf("Algoritmos e ");
   printf("Prog. II\n");
-  printf("Turmas: 02N..... 02P\n");
+  printf("Turmas: 02N.... 02P\n");
   return 0;
 }
