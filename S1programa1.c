ba6cb8581c2217e1f17afad6ebb87fa12d405cb0
LipeCortez <75758075+LipeCortez@users.noreply.github.com>
2024-02-21 20:30:59 -0300

    Create S1programa1.c

---
+++ S1programa1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  // 1º aula de APII
+  // Imprime algumas mensagens na tela
+  printf("Mackenzie - FCI\n");
+  printf("Algoritmos e ");
+  printf("Prog. II\n");
+  printf("Turmas: 02N 02P\n");
+  return 0;
+}
